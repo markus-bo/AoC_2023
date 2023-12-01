fc21f57fae753777f755c11b533c93ba7b983a3c
Markus Bodlos <74670566+markus-bo@users.noreply.github.com>
2023-12-01 08:49:06 +0100

    Projektdateien hinzufügen.

---
+++ AoC_2023.Day1/Program.cs
@@ -0,0 +1,58 @@
+﻿using AoC_Toolbox;
+using AoC_Toolbox.Geometry;
+using AoC_Toolbox.InputParsing;
+using AoC_Toolbox.Pathfinding;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        PuzzleSetup.Solve(solutionPart1, solutionPart2);
+    }
+
+    static object? solutionPart1(string[] input)
+    {
+        var result = input.Select(l => (a: l.First(c => char.IsDigit(c)), b: l.Last(c => char.IsDigit(c))))
+             .Select(l => (l.a - '0') * 10 + (l.b - '0'))
+             .Sum();
+
+        return result;
+    }
+
+    static object? solutionPart2(string[] input)
+    {
+        var pattern = @"(one|two|three|four|five|six|seven|eight|nine|\d{1})";
+        var numberStrings = new List<string>() { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        var sum = 0;
+
+        foreach(var l in input)
+        {
+            MatchCollection matches = Regex.Matches(l, pattern, RegexOptions.Compiled);
+
+            var first = matches.First().Value;
+            var last = matches.Last().Value;
+
+            var a = 0;
+            var b = 0;
+
+            if (first.Length == 1)
+                a = first[0] - '0';
+            else
+                a = numberStrings.IndexOf(first);
+
+            if (last.Length == 1)
+                b = last[0] - '0';
+            else
+                b = numberStrings.IndexOf(last);
+
+
+            sum += a * 10 + b;
+        }
+
+        return sum;
+    }
+}
---
+++ AoC_Toolbox/Pathfinding/IPathfindingAStar.cs
@@ -0,0 +1,6 @@
+﻿namespace AoC_Toolbox.Pathfinding;
+
+public interface IPathfindingAStar<T> where T : Node
+{
+    public long searchMinimumPathLength(T startNode, T endNode, Func<T, T, long> heuristic, long initialLength = 0);
+}
