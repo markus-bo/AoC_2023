76e3805f86cf300779bf3c9529c1cdddbbcdc1f3
Markus Bodlos <74670566+markus-bo@users.noreply.github.com>
2023-12-08 10:23:03 +0100

    Add Arithmetic functions (LCM, GCD) to toolbox

---
+++ AoC_Toolbox/Arithmetic/Numbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Toolbox
+{
+    public static class Arithmetic
+    {
+        public static long LCM(long[] numbers)
+        {
+            long lcm = 1L;
+
+            foreach (long number in numbers)
+            {
+                lcm = LCM(lcm, number);
+            }
+
+            return lcm;
+        }
+
+        public static long LCM(long a, long b)
+        {
+            return (a * b) / GCD(a, b);
+        }
+
+        public static long GCD(long a, long b)
+        {
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+    }
+}
