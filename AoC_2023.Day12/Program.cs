99e4997f5d93f33e72f4b380ae068b13e7b2fd90
Markus Bodlos <markus.bodlos@outlook.com>
2023-12-14 09:35:32 +0100

    So far day 12

--- AoC_2023.Day12/Program.cs
+++ AoC_2023.Day12/Program.cs
@@ -1,20 +1,28 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using AoC_Toolbox;
 using AoC_Toolbox.Geometry;
 using AoC_Toolbox.InputParsing;
 using AoC_Toolbox.Pathfinding;
+using Microsoft.VisualBasic;
 
 internal class Solution
 {
     static void Main(string[] args)
     {
+
         PuzzleSetup.Solve(solutionPart1, solutionPart2);
+
+       
     }
 
     static object? solutionPart1(string[] input)
     {
+        var stopWatch = new Stopwatch();
+        
+        stopWatch.Start();
         var result = 0;
-
+        var first = true;
         foreach(var line in input)
         {
             var pattern = line.Split()[0];
@@ -23,25 +31,177 @@ internal class Solution
                                         .ToArray();
 
             memoization.Clear();
-            
-            var combinations = GetAllCombinations(0, pattern, "", groups);
 
+            var combinations = GetAllCombinations2(0, pattern, "", groups);
+            
+            /*var combinations = GetAllCombinations(0, 0, pattern, "", groups,
+                                                    groups.Sum(x => x),
+                                                    pattern.Count(x => x=='#'),
+                                                    pattern.Count(x => x=='?'),
+                                                    groups.Length - 1,
+                                                    pattern.Count(x => x=='.'));*/
+
+            if (first)
+            {
+                first = false;
+
+                //Console.WriteLine($"{recursionCalls} {validPermutations} {invalidPermutations}");
+            }
             result += combinations;
+
+            Console.WriteLine($"  --> {combinations}");
+            Console.ReadLine();
         }
 
+         stopWatch.Stop();
+        Console.Error.WriteLine(stopWatch.Elapsed);
         return result;
     }
 
-    static Dictionary<(int, string), int> memoization = new Dictionary<(int, string), int>();
-
-    static int GetAllCombinations(int index, string target, string current, int[] groups)
+    static Dictionary<string, int> memoization = new Dictionary<string, int>();
+    
+    static int recursionCalls = 0;
+     static int validPermutations = 0;   
+     static int invalidPermutations = 0;   
+    static int GetAllCombinations(int index, int indexGroup, string target, string current, int[] groups, int neededOnes, int countedOnes, int unassignedQuestionMarks, int neededZeros, int countedZeros)
     {
-        if (memoization.ContainsKey((index, current)))
+
+      /*  if (memoization.ContainsKey(index))
         {
-            return memoization[(index, current)];
+            return memoization[index];
+        }*/
+
+            //Console.Error.WriteLine($"{current} {neededOnes} {countedOnes} {unassignedQuestionMarks}");
+        recursionCalls++;
+
+        if (countedOnes > neededOnes)
+        {
+           return 0;
         }
 
-        if (index == target.Length)
+        if (countedZeros + unassignedQuestionMarks < neededZeros)
+        {
+           //return 0;
+        }
+
+        if (countedOnes + unassignedQuestionMarks < neededOnes)
+        {
+          //return 0;
+        }
+
+        if (index == target.Length || indexGroup == groups.Length)
+        {
+            if (current.Split('.')
+                      .Where(x => x != "")
+                      .Select(x => x.Length)
+                      .SequenceEqual(groups))
+                      {
+                         validPermutations++;
+                         //Console.Error.WriteLine($"{target} {current}");
+                         //Console.Error.WriteLine($"  valid");
+                        return 1;
+                       
+                      }
+                      else
+                      {
+                        
+                        invalidPermutations++;
+                        //Console.Error.WriteLine($"  invalid 1");
+                        return 0;
+                      }
+        }
+
+        
+
+        var combinations = 0;
+
+    
+        if (target[index] == '.')
+        {
+            combinations += GetAllCombinations(index + 1, indexGroup, target, current + target[index], groups, neededOnes, countedOnes, unassignedQuestionMarks, neededZeros, countedZeros);
+        }
+        else
+        {        
+            if (target[index] == '?')
+            {
+                combinations += GetAllCombinations(index + 1, indexGroup, target, current + '.', groups, neededOnes, countedOnes, unassignedQuestionMarks-1, neededZeros, countedZeros+1);
+            }
+
+                var questionMarkCount = 0;
+                var hashTagCount = 0;
+
+                if (current.Length + groups[indexGroup] > target.Length)
+                {
+                    //Console.Error.WriteLine($"  invalid 2");
+                    //memoization.Add(index, combinations);
+                    return combinations;
+                }
+
+                for (int i=index; i<index+groups[indexGroup]; i++)
+                {
+                    if (target[i] == '.')
+                    {
+                        //Console.Error.WriteLine($"  invalid 3");
+                        //memoization.Add(index, combinations);
+                        return combinations;
+                    }
+                    else if (target[i] == '?')
+                    {
+                        questionMarkCount++;
+                        hashTagCount++;
+                    }
+                    else if (target[i] == '#')
+                    {
+                        //hashTagCount++;
+                    }
+                }
+
+                var addZero = false;
+                if (indexGroup < groups.Length - 1)
+                {
+                    if (index+groups[indexGroup] >= target.Length)
+                    {
+                        //Console.Error.WriteLine($"  invalid 4");
+                       // memoization.Add(index, combinations);
+                        return combinations;
+                    }
+
+                    if (target[index+groups[indexGroup]] == '#')
+                    {
+                        //Console.Error.WriteLine($"  invalid 5 {current} - {target}");
+                       // memoization.Add(index, combinations);
+                        return combinations;
+                    }
+                    else if (target[index+groups[indexGroup]] == '?')
+                    {
+                        questionMarkCount++;
+                    }
+
+                    addZero = true;
+                }
+            
+
+                combinations += GetAllCombinations(index + groups[indexGroup] + (addZero ? 1 : 0), indexGroup + 1, target, current + new string('#', groups[indexGroup]) + (addZero ? "." : ""), groups, neededOnes, countedOnes + hashTagCount, unassignedQuestionMarks-questionMarkCount, neededZeros, countedZeros+(addZero ? 1 : 0));
+                
+
+                //combinations += GetAllCombinations(index + 1, target, current + '#', groups, neededOnes, countedOnes+1, unassignedQuestionMarks-1, neededZeros, countedZeros);
+        }
+
+        //memoization.Add((index, current), combinations);
+
+        //memoization.Add(index, combinations);
+        return combinations;
+
+    }
+
+    static int GetAllCombinations2(int index, string target, string current, int[] groups)
+    {
+        if (memoization.ContainsKey(index.ToString()))
+        {
+            return memoization[index.ToString()];
+        }
+
+        if (index >= target.Length)
         {
             if (current.Split('.')
                       .Where(x => x != "")
@@ -56,29 +216,36 @@ internal class Solution
                       }
         }
 
+        
+
         var combinations = 0;
 
         if (target[index] == '?')
         {
-            combinations += GetAllCombinations(index + 1, target, current + '#', groups);
-            combinations += GetAllCombinations(index + 1, target, current + '.', groups);
+            combinations += GetAllCombinations2(index + 1, target, current +'#', groups);
+            combinations += GetAllCombinations2(index + 1, target, current + '.', groups);
         }
         else
         {
-            combinations += GetAllCombinations(index + 1, target, current + target[index], groups);
+            combinations += GetAllCombinations2(index + 1, target, current + target[index], groups);
         }
 
-        memoization.Add((index, current), combinations);
-
+        memoization.Add(index.ToString(), combinations);
         return combinations;
 
     }
+
     static object? solutionPart2(string[] input)
     {
-        var result = 0;
+        var stopWatch = new Stopwatch();
+        
+        stopWatch.Start();
+        var result = 0L;
 
+        var j=0;
         foreach(var line in input)
         {
+            Console.Error.WriteLine(j++);
             var pattern = line.Split()[0];
             var groups = line.Split()[1].Split(',')
                                         .Select(int.Parse)
@@ -91,18 +258,27 @@ internal class Solution
             {
                 unfoldedPattern += pattern + "?";
 
+
                 unfoldedGroups.AddRange(groups);
             }
 
+            unfoldedPattern = new string(unfoldedPattern[..^1]);
+
             //Console.Error.WriteLine($"{unfoldedPattern} {string.Join(',', unfoldedGroups)}");
 
-            var combinations = GetAllCombinations(0, unfoldedPattern, "", unfoldedGroups.ToArray());
+            memoization.Clear();
 
-            Console.Error.WriteLine($"{pattern} {combinations}");
+            var combinations = (long)GetAllCombinations2(0, unfoldedPattern, "", unfoldedGroups.ToArray());
 
+           Console.Error.WriteLine($"{pattern} {combinations}");
+
+           // Console.ReadLine();
             result += combinations;
         }
 
+        stopWatch.Stop();
+        Console.Error.WriteLine(stopWatch.Elapsed);
+        Console.Error.WriteLine($"{result}");
         Console.ReadLine();
         
         return result;
---
+++ AoC_2023.Day13/Program.cs
@@ -0,0 +1,195 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using AoC_Toolbox;
+using AoC_Toolbox.Geometry;
+using AoC_Toolbox.InputParsing;
+using AoC_Toolbox.Pathfinding;
+using Microsoft.VisualBasic;
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        PuzzleSetup.Solve(solutionPart1, solutionPart2);
+    }
+
+    static IEnumerable<int> GetPossibleMirrorPositions(string line)
+    {
+
+        for (int i=0; i<line.Length-1; i++)
+        {
+            if (line[i] == line[i+1])
+            {
+                yield return i;
+            }
+        }
+    }
+
+    static int GetVerticalMirror(string[] map)
+    {
+
+        //Console.Error.WriteLine("vertical");
+        var possibilities = new Dictionary<int, bool>();
+
+        for(int i=0; i<map[0].Count(); i++)
+        {
+            possibilities.Add(i, true);
+        }
+
+        foreach(var line in map)
+        {
+            var linePossibilities = GetPossibleMirrorPositions(line).ToList();
+
+            for(int i=0; i<map[0].Count(); i++)
+            {
+                if (!linePossibilities.Contains(i))
+                    possibilities[i] = false;
+            }
+
+            //Console.Error.WriteLine($"{line} -- {string.Join(" ", GetPossibleMirrorPositions(line))}");
+        }
+
+        //debug
+        foreach(var p in possibilities)
+        {
+            //Console.Error.WriteLine($"{p.Key} {p.Value}");
+        }
+
+        if (possibilities.Any(x => x.Value == true)  == false)
+        {
+            return 0;
+        }
+
+        if (possibilities.Count(x => x.Value == true) > 1)
+        {
+            return 0;
+        }
+
+        var indexSplit = possibilities.Where(x => x.Value == true).Select(x => x.Key).First();
+
+        foreach(var line in map)
+        {
+            var left = new string(line[..(indexSplit+1)]);
+            var right = new string(line[(indexSplit+1)..]);
+
+
+            //Console.Error.WriteLine($"{new string(left)} {new string(right)}");
+            if (left.Length < right.Length)
+            {
+                if (right.StartsWith(new string(left.Reverse().ToArray())) == false)
+                {
+                    //Console.Error.WriteLine("wrong 1");
+                    return 0;
+                }
+            }
+            else
+            {
+                if (left.EndsWith(new string(right.Reverse().ToArray())) == false)
+                {
+                    //Console.Error.WriteLine("Wrong 2");
+                    return 0;
+                }
+            }
+            
+        }
+
+        return indexSplit+1;
+    }
+
+    static int GetHoricontalMirror(string[] map)
+    {
+        //Console.Error.WriteLine($"horicontal {map[0]}");
+        var possibilities = new Dictionary<int, bool>();
+
+        for(int i=0; i<map.Length; i++)
+        {
+            possibilities.Add(i, true);
+        }
+
+        for(int j=0; j<map[0].Length; j++)
+        {
+            var line = new StringBuilder();
+
+            for(int i=0; i<map.Length; i++)
+            {
+                line.Append(map[i][j]);
+            }
+
+            var linePossibilities = GetPossibleMirrorPositions(line.ToString()).ToList();
+
+            for(int i=0; i<map.Length; i++)
+            {
+                if (!linePossibilities.Contains(i))
+                    possibilities[i] = false;
+            }
+
+            //Console.Error.WriteLine($"horicontal {line.ToString()} -- {string.Join(" ", GetPossibleMirrorPositions(line.ToString()))}");
+        }
+
+        //debug
+        foreach(var p in possibilities)
+        {
+            //Console.Error.WriteLine($"{p.Key} {p.Value}");
+        }
+
+        if (possibilities.Any(x => x.Value == true)  == false)
+        {
+            return 0;
+        }
+
+        if (possibilities.Count(x => x.Value == true) > 1 && possibilities.Count(x => x.Value == true) < 5)
+        {
+            Console.Error.WriteLine($"here");
+            return 0;
+        }
+        
+        if (possibilities.Count(x => x.Value == true) > 1)
+        {
+            return 0;
+        }
+
+
+        return possibilities.Where(x => x.Value == true).Select(x => x.Key).First() + 1;
+    }
+
+    static int GetValue(string[] map)
+    {
+        var a = GetVerticalMirror(map);
+        var b = 0;
+
+        if (a==0)
+        b  =GetHoricontalMirror(map);
+
+        //Console.Error.WriteLine($"{a} {b}");
+        return a + b * 100;
+    }
+
+    static object? solutionPart1(string[] input)
+    {
+        var map = new List<string>();
+        var result = 0;
+        
+        for(int i=0; i<input.Length; i++)
+        {
+            if (input[i] =="")
+            {
+                result += GetValue(map.ToArray());
+                map.Clear();
+            }
+            else
+            {
+                map.Add(input[i]);
+            }
+        }
+
+        result += GetValue(map.ToArray());
+        
+        Console.ReadLine();
+        return result;
+    }
+
+    static object? solutionPart2(string[] input)
+    {
+        return null;
+    }
+}
