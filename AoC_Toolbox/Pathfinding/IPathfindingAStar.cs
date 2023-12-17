3c0f6b9cae09220d2238672e820d09c39990104d
Markus Bodlos <74670566+markus-bo@users.noreply.github.com>
2023-12-17 20:28:10 +0100

    Add customizable pathfound condition for astar

--- AoC_Toolbox/Pathfinding/IPathfindingAStar.cs
+++ AoC_Toolbox/Pathfinding/IPathfindingAStar.cs
@@ -2,5 +2,5 @@
 
 public interface IPathfindingAStar<T> where T : Node
 {
-    public long searchMinimumPathLength(T startNode, T endNode, Func<T, T, long> heuristic, Func<T, T, bool> equalityComparer, long initialLength = 0);
+    public long searchMinimumPathLength(T startNode, T endNode, Func<T, T, long> heuristic, Func<T, T, bool> pathFoundCondition, long initialLength = 0);
 }
